9742efd856081601f8031f26e8998ee5319cc7a9
pedrosauce <petermcdermott36@gmail.com>
2017-03-10 00:10:44 +0000

    Huffman encoding
    Signed-off-by: pedrosauce <petermcdermott36@gmail.com>

---
+++ Node.java
@@ -0,0 +1,10 @@
+package labs;
+class Node
+{
+
+public char letter='@';            //stores letter
+
+public Node leftChild;         // this node's left child
+public Node rightChild;        // this node's right child
+
+}  // end 
